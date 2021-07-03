8031990c96e8bc945ce5169df12d692f83367af3
MadScientist2854 <madscientist2854@gmail.com>
2021-07-03 11:11:08 +0500

    added fn symbols

--- src/linker.h
+++ src/linker.h
@@ -9,17 +9,47 @@ bfd create_bfd(JovisIR ir) {
     asection *code = bfd_make_section_old_way(&abfd, ".code");
 
     // create object-file symbols for code and data section starting points TODO
+    // check how relocations are done to make sure this is needed
+
+    typedef struct SymLLNode {
+        asymbol *sym;
+        struct SymLLNode *next;
+    } SymLLNode;
+    SymLLNode *last;
+    SymLLNode *first = last;
 
     // loop through fns
     Fn *cur_fn = ir.code;
-    while (cur_fn->next != NULL) {
+    int count = 0;
+    for (;cur_fn->next != NULL; count++) {
         // for each fn, assemble it's code
         MCode mcode = j_assemble(cur_fn->text, cur_fn->asm_type);
+        // add symbol that points to start of fn
+        asymbol *fn_sym = bfd_make_empty_symbol(&abfd);
+        sprintf(fn_sym->name, "f%d", count);
+        fn_sym->section = code;
+        fn_sym->flags = BSF_LOCAL;
+        fn_sym->value = code->size;
+
+        last->sym = fn_sym;
+        SymLLNode *next = malloc(sizeof(SymLLNode));
+        last->next = next;
+        last = next;
         // put the machine code into the .code section
         bfd_set_section_contents(&abfd, code, mcode.encode, code->size, mcode.size);
-        // add symbol that points to start of fn TODO
         cur_fn = cur_fn->next;
     }
+    last->sym = (asymbol *)0;
+    last->next = NULL;
+    // turn linked list into array
+    asymbol *sym_tab[count+1];
+    SymLLNode *cur = first;
+    for (size_t i = 0; i <= count; i++) {
+        sym_tab[i] = cur;
+        cur = cur->next;
+    }
+    // set sym tab on bfd
+    bfd_set_symtab(&abfd, sym_tab, count);
     // add bfd_error checks TODO
 
     return abfd;
